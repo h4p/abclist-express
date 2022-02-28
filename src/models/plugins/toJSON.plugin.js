fd7f2d230a9b063ef80178f4155cd8654a08c704
Tobias Wessels <tobias.wessels@ongoingmedia.de>
2022-02-28 14:39:45 +0100

    Add _id and timestamps to json

--- src/models/plugins/toJSON.plugin.js
+++ src/models/plugins/toJSON.plugin.js
@@ -29,10 +29,10 @@ const toJSON = (schema) => {
       });
 
       ret.id = ret._id.toString();
-      delete ret._id;
+      // delete ret._id;
       delete ret.__v;
-      delete ret.createdAt;
-      delete ret.updatedAt;
+      // delete ret.createdAt;
+      // delete ret.updatedAt;
       if (transform) {
         return transform(doc, ret, options);
       }
